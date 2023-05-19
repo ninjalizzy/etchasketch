c4be4d9efea446af72810d6ccbd9dec457b7a0ff
Lizzy <nynjalizzy@gmail.com>
2023-05-18 20:43:12 -0700

    Color in boxes on mouseover

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const container = document.querySelector('.container');
 
 for (let x = 0; x < 16; x++) {
     for (let y = 0; y < 16; y++) {
-        let box = document.createElement('div');
+        box = document.createElement('div');
         box.classList.add('boxes');
         container.appendChild(box);
     };
 };
+const boxes = document.querySelectorAll('.boxes');
+
+let changeColor = (e) => {
+    e.target.style.backgroundColor = "blue";
+};
+
+boxes.forEach((item) => {
+    item.addEventListener('mouseover', changeColor);
+});
